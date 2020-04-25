1c1a68a63b92fca479f5ea6c0868fba4d2ec97cf
Yuki Ishitsuka <yuki-ishitsuka@lookingup.co.jp>
2020-04-25 23:10:14 +0900

    add types

---
+++ google-home-notifier.d.ts
@@ -0,0 +1,5 @@
+export declare const device: (name: string, lang?: string) => void;
+export declare const ip: (ip: string) => void;
+export declare const accent: (accent: string) => void;
+export declare const notify: (text: string, callback: (res: string) => void) => void;
+export declare const play: (mp3Url: string, callback: (res: string) => void) => void;
